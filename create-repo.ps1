70deef5faa2d801b072c4dd2a6ef3d8783a8877d
RonaldOutline <85238050+RonaldOutline@users.noreply.github.com>
2026-04-30 12:42:10 +0300

    Update service page and site language

---
+++ create-repo.ps1
@@ -0,0 +1,11 @@
+﻿$headers = @{
+    "Authorization" = "token $env:GITHUB_TOKEN"
+    "Accept" = "application/vnd.github.v3+json"
+}
+$body = @{
+    "name" = "sps-aprill-2026"
+    "private" = $false
+} | ConvertTo-Json
+
+Invoke-RestMethod -Uri "https://api.github.com/user/repos" -Method Post -Headers $headers -Body $body
+
---
+++ list-repos.ps1
@@ -0,0 +1,7 @@
+﻿$headers = @{
+    "Authorization" = "token $env:GITHUB_TOKEN"
+    "Accept" = "application/vnd.github.v3+json"
+}
+
+$result = Invoke-RestMethod -Uri "https://api.github.com/user/repos?per_page=100" -Method Get -Headers $headers
+$result | ForEach-Object { $_.full_name }
